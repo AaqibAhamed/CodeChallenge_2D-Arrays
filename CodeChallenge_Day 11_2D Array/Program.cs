a15412d01020a2f2e06305c91c1a2885b5420bc8
Ahamed Aaqib <ahamedaaqib96@outlook.com>
2020-04-19 15:05:03 +0530

    Add project files.

---
+++ CodeChallenge_Day 11_2D Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge_Day_11_2D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] myArr = new int[6][];
+
+            for(int i=0;i<6;i++)
+            {
+                myArr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+           
+            Console.ReadLine();
+
+        }
+    }
+}
